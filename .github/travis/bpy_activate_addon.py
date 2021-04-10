94e0c31b4606265f3acd0db6ea5f31a7ee132e0c
Josue-Martinez-Moreno <josue.martinezmoreno@anu.edu.au>
2021-04-10 18:36:03 +1000

    Catch errors for travis

--- .github/travis/bpy_activate_addon.py
+++ .github/travis/bpy_activate_addon.py
@@ -1,5 +1,12 @@
 import bpy
+import sys
 
 # Register the addon and enable it
-bpy.ops.preferences.addon_install(filepath='./blendernc.zip')
-bpy.ops.preferences.addon_enable(module='blendernc')
+try:
+    bpy.ops.preferences.addon_install(filepath='./blendernc.zip')
+except: #Generic excepts to catch any error 
+    sys.exit(1)
+try:
+    bpy.ops.preferences.addon_enable(module='blendernc')
+except: #Generic excepts to catch any error 
+    sys.exit(2)
