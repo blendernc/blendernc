40a07f9ddbe30cbeb29868def18d36efaeb2e0f1
Josue-Martinez-Moreno <josue.martinezmoreno@anu.edu.au>
2021-04-10 14:38:43 +1000

    Zip cloned file for CI build test.

--- .github/travis/bpy_activate_addon.py
+++ .github/travis/bpy_activate_addon.py
@@ -2,4 +2,4 @@ import bpy
 
 # Register the addon and enable it
 bpy.ops.preferences.addon_install(filepath='./blendernc.zip')
-bpy.ops.preferences.addon_enable(module='blendernc-dev')
+bpy.ops.preferences.addon_enable(module='blendernc')
