f3c2107c14f4aac77de5cb5ef82cd01d8e82320c
Tisham Dhar <whatnickd@gmail.com>
2021-05-29 17:08:43 +1000

    #44 Turn off progrss bar on pip
    Signed-off-by: Tisham Dhar <whatnickd@gmail.com>

--- .github/travis/build-test.sh
+++ .github/travis/build-test.sh
@@ -2,5 +2,5 @@
 
 $BLENDERPY -m ensurepip --default-pip
 
-$BLENDERPY -m pip install -r requirements.txt
+$BLENDERPY -m pip install -r requirements.txt --progress-bar off
 blender --background --python ./bpy_activate_addon.py
