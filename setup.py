e007ac630dd25a589b1c5a6bf8f8ab08bd92a7fa
Josue-Martinez-Moreno <josue.martinezmoreno@anu.edu.au>
2021-06-05 02:33:06 +1000

    Fix setup.py for pipy release

--- setup.py
+++ setup.py
@@ -1,30 +1,30 @@
 #!/usr/bin/env python3
-from distutils.extension import Extension
+# from distutils.extension import Extension
 
 # TODO: Fix issue with blender not having python headers.
 # from Cython.Build import cythonize
-from Cython.Distutils import build_ext
+# from Cython.Distutils import build_ext
 from setuptools import setup
 
-extensions = Extension(
-    name="cython_build.lic_internal",
-    sources=["./blendernc/core/lic/lic_internal.pyx"],
-)
+# extensions = Extension(
+#     name="cython_build.lic_internal",
+#     sources=["./blendernc/core/lic/lic_internal.pyx"],
+# )
 
 
-class CustomBuildExtCommand(build_ext):
-    """build_ext command for use when numpy headers are needed."""
+# class CustomBuildExtCommand(build_ext):
+#     """build_ext command for use when numpy headers are needed."""
 
-    def run(self):
+#     def run(self):
 
-        # Import numpy here, only when headers are needed
-        import numpy
+#         # Import numpy here, only when headers are needed
+#         import numpy
 
-        # Add numpy headers to include_dirs
-        self.include_dirs.append(numpy.get_include())
+#         # Add numpy headers to include_dirs
+#         self.include_dirs.append(numpy.get_include())
 
-        # Call original build_ext command
-        build_ext.run(self)
+#         # Call original build_ext command
+#         build_ext.run(self)
 
 
 setup(
