7efa578d5d8c81ae2a454165ab26e55ad7af2925
Josue-Martinez-Moreno <josue.martinezmoreno@anu.edu.au>
2021-06-11 18:38:09 +1000

    Fix 'object is not an icosphere' and object selection. Active object has preference over selected object.

--- blendernc/operators.py
+++ blendernc/operators.py
@@ -4,6 +4,7 @@ from os.path import abspath
 
 import bpy
 
+from ..blendernc.msg_errors import active_selection_preference, unselected_object
 from .python_functions import (
     BlenderncEngine,
     get_node,
@@ -245,9 +246,18 @@ class BlenderNC_OT_apply_material(bpy.types.Operator):
         # Check if an object is selected or picked.
         if not sel_obj and act_obj:
             sel_obj = act_obj
-        elif sel_obj or act_obj:
+        elif sel_obj and not act_obj:
             pass
+        elif sel_obj and act_obj:
+            if sel_obj.name != act_obj.name:
+                bpy.context.window_manager.popup_menu(
+                    active_selection_preference, title="Warning", icon="INFO"
+                )
+                sel_obj = act_obj
         else:
+            bpy.context.window_manager.popup_menu(
+                unselected_object, title="Error", icon="ERROR"
+            )
             return {"FINISHED"}
 
         blendernc_materials = [
@@ -281,7 +291,7 @@ class BlenderNC_OT_apply_material(bpy.types.Operator):
 
         P_BSDF = blendernc_material.node_tree.nodes.get("Principled BSDF")
 
-        if act_obj.name == "Icosphere" or sel_obj.name == "Icosphere":
+        if sel_obj.name == "Icosphere":
             texcoord_link = texcoord.outputs.get("Generated")
             imagetex.projection = "SPHERE"
         else:
