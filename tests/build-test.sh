1c189e558f88f52287b29f706344e1dcb875bcf2
Josue-Martinez-Moreno <josue.martinezmoreno@anu.edu.au>
2021-07-01 17:46:55 +1000

    Fix coverage with different blender versions.

--- tests/build-test.sh
+++ tests/build-test.sh
@@ -8,6 +8,9 @@ $BLENDERPY -m pip install coverage --progress-bar off
 
 $BLENDERPY -m pip install -e . --progress-bar off
 
+blender_version=$(blender --version | head -n 1)
+echo ${blender_version}
+
 COVERAGE_PROCESS_START=${PWD}"/.coveragerc"
 export COVERAGE_PROCESS_START=$COVERAGE_PROCESS_START
 export PYTHONPATH=$PYTHONPATH:${PWD}
@@ -26,4 +29,3 @@ rm *.png
 
 coverage combine
 coverage report
-coverage xml
