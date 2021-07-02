5542f159093e25ef3593caf3a55ffd7d61fcca54
Josue-Martinez-Moreno <josue.martinezmoreno@anu.edu.au>
2021-07-03 02:00:05 +1000

    Fix moving coverage files.

--- tests/coverage-combine.sh
+++ tests/coverage-combine.sh
@@ -2,13 +2,10 @@
 
 $BLENDERPY -m pip install coverage --progress-bar off
 
-ls -la .
 # Navegate to test
 cd ./tests/
-ls -la .
 # Move all coverage files from within default download-artifact structure
-ls -la ./coverage_*
-mv ./coverage_*/* .
+mv ./coverage_*/.coverage* .
 # Combine, create coverage.xml and report coverage.
 coverage combine .coverage_Blender*
 coverage report
