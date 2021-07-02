3663f6ad873f258d6d0721d603164d6ed0f62a21
Josue-Martinez-Moreno <josue.martinezmoreno@anu.edu.au>
2021-07-03 01:34:59 +1000

    Fix wildcard when moving coverage files.

--- tests/coverage-combine.sh
+++ tests/coverage-combine.sh
@@ -5,7 +5,7 @@ $BLENDERPY -m pip install coverage --progress-bar off
 # Navegate to test
 cd ./tests/
 # Move all coverage files from within default download-artifact structure
-mv ./coverage_/* .
+mv ./coverage_*/* .
 # Combine, create coverage.xml and report coverage.
 coverage combine .coverage_Blender*
 coverage report
