f21d017fd770a5cd9c0e26d4eff1dc6d7c9f4955
Josue-Martinez-Moreno <josue.martinezmoreno@anu.edu.au>
2021-07-03 01:47:33 +1000

    Fix coverage and  download-artifact structure.

--- tests/coverage-combine.sh
+++ tests/coverage-combine.sh
@@ -2,9 +2,12 @@
 
 $BLENDERPY -m pip install coverage --progress-bar off
 
+ls -la .
 # Navegate to test
 cd ./tests/
+ls -la .
 # Move all coverage files from within default download-artifact structure
+ls -la ./coverage_*
 mv ./coverage_*/* .
 # Combine, create coverage.xml and report coverage.
 coverage combine .coverage_Blender*
